0ae2d05da9ed06deac3dc0e2e6223f803be6e1f2
kalashnikovaYla <kalashnikova-yla@mail.ru>
2024-11-28 10:32:57 +0500

    Initial Commit

---
+++ TeaTimer/TeaTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeaTimerApp.swift
+//  TeaTimer
+//
+//  Created by Юлия Калашникова on 28.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct TeaTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
